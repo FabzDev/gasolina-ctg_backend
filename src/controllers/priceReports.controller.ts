6153f9d92101185d700ab3566077d6cf3d9d09e7
FabzDev <fabztech@outlook.com>
2026-05-24 15:01:19 -0500

    Prima ts file removed, priceReports controller completed

--- src/controllers/priceReports.controller.ts
+++ src/controllers/priceReports.controller.ts
@@ -2,8 +2,8 @@ import { Request, Response } from 'express';
 import db from '../db/gasolinaCTG.db';
 
 export const getReports = async (req: Request, res: Response) => {
-  try {
-    const result = await db.query(`
+    try {
+        const result = await db.query(`
         SELECT 
           pr.id,
           gs.name AS station,
@@ -19,53 +19,60 @@ export const getReports = async (req: Request, res: Response) => {
         ORDER BY pr.reported_at DESC
       `);
 
-    res.json({
-      success: true,
-      data: result.rows,
-    });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({
-      success: false,
-      message: 'Error fetching reports',
-    });
-  }
+        res.json({
+            success: true,
+            data: result.rows,
+        });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({
+            success: false,
+            message: 'Error fetching reports',
+        });
+    }
 };
 
-export const createReport = async (req: Request, res: Response) => {
-  try {
-    const result = await db.query(`
-      INSERT INTO gas_stations (
-        name,
-        address,
-        neighborhood,
-        city,
-        latitude,
-        longitude,
-        brand,
-        created_at
-      )
-      VALUES (
-        'Estación Terpel La Cordialidad - Sector Terminal',
-        'Vía La Cordialidad cerca al acceso de la Terminal de Transportes de Cartagena',
-        'Pozón - Doña Manuela',
-        'Cartagena',
-        10.4169,
-        -75.5208,
-        'Terpel',
-        NOW()
-      );
-    `);
 
-    res.json({
-      success: true,
-      data: result.rows,
-    });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({
-      success: false,
-      message: 'Error fetching stations',
-    });
-  }
+export const createReport = async (req: Request, res: Response) => {
+    try {
+        const { 
+          gas_station_id,
+          fuel_type_id,
+          user_id,
+          price
+        } = req.body;
+
+        // VALIDACIÓN SUPER BÁSICA
+        if (!gas_station_id || !fuel_type_id || !user_id || !price) {
+            return res.status(400).json({
+                success: false,
+                message: 'Missing required fields',
+            });
+        }
+
+        const result = await db.query(
+            `
+      INSERT INTO price_reports (
+        gas_station_id,
+        fuel_type_id,
+        user_id,
+        price
+      )
+      VALUES ($1,$2,$3,$4)
+      RETURNING *
+    `,
+            [gas_station_id, fuel_type_id, user_id, price],
+        );
+
+        res.status(201).json({
+            success: true,
+            data: result.rows[0],
+        });
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({
+            success: false,
+            message: 'Error creating price report',
+        });
+    }
 };
